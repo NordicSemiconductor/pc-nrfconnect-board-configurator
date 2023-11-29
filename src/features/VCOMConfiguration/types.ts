dcc64658403f087fda2e9ced6f0569269451e6f3
Oystein Steimler <oystein@nyvri.net>
2023-11-29 10:56:32 +0100

    Add copyright headers

--- src/features/VCOMConfiguration/types.ts
+++ src/features/VCOMConfiguration/types.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 export interface VcomConfig {
     vcomId: string;
     vcomEnable: boolean;
