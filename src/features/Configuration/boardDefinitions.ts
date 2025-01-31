98240e379e13e037fe2e4ed1b8611fb420e155c7
Oystein Steimler <oystein@nyvri.net>
2025-01-31 12:33:14 +0100

    Support for nRF54L20

--- src/features/Configuration/boardDefinitions.ts
+++ src/features/Configuration/boardDefinitions.ts
@@ -15,6 +15,7 @@ import nrf54l15v020json from '../../common/boards/nrf_PCA10156_0.2.0.json';
 import nrf54l15v030json from '../../common/boards/nrf_PCA10156_0.3.0.json';
 import nrf9151v020json from '../../common/boards/nrf_PCA10171_0.2.0_9151.json';
 import nrf54h20v070json from '../../common/boards/nrf_PCA10175_0.7.0_54H20.json';
+import nrf54l20v010json from '../../common/boards/nrf_PCA10184_0.1.0_54L20.json';
 
 export type BoardDefinition = {
     boardControllerConfigDefinition?: BoardControllerConfigDefinition;
@@ -36,6 +37,8 @@ const typednrf54h20json = nrf54h20pdk080json as BoardControllerConfigDefinition;
 const typednrf54h20v070json =
     nrf54h20v070json as BoardControllerConfigDefinition;
 const typednrf9151v020json = nrf9151v020json as BoardControllerConfigDefinition;
+const typednrf54l20v010json =
+    nrf54l20v010json as BoardControllerConfigDefinition;
 
 export function getBoardDefinition(
     device: Device,
@@ -87,6 +90,10 @@ export function getBoardDefinition(
         case 'PCA10175':
             // nRF54H20
             return { boardControllerConfigDefinition: typednrf54h20v070json };
+
+        case 'PCA10184':
+            // nRF54L20
+            return { boardControllerConfigDefinition: typednrf54l20v010json };
 
         default:
             return { controlFlag: { unrecognizedBoard: true } };
